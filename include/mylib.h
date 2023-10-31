0b9cd2fcf103f7d9a5a84e1869deaa173a111b06
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2023-10-31 14:57:21 -0300

    featupdated API and added Block definition

--- include/mylib.h
+++ include/mylib.h
@@ -1,6 +1,14 @@
 #include <stddef.h>
 
-#define SIZE 128
+#define SIZE 10
+#define TRUE 1
+#define FALSE 0
 
-void *my_malloc(size_t size);
-void my_free(void *ptr);
+typedef struct Block {
+    size_t size;
+    struct Block* next;
+} Block;
+
+void *myMalloc(size_t size);
+void myFree(void *ptr);
+void initializeHeap();

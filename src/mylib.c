9f31291c6901f6a2672962d42432f7a20caf80a5
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2023-11-02 15:42:25 -0300

    feat: added malloc method

--- src/mylib.c
+++ src/mylib.c
@@ -1,15 +1,48 @@
 #include "mylib.h"
 
-char buffer[SIZE];
+char heap[MEM_SIZE];
 
-char* __my_heap_start = buffer;
-char* __my_heap_end = buffer + SIZE;
+void *myMalloc(size_t size) {
+    static int heapHasBeenInitialized = FALSE;
 
-void *my_malloc(size_t size) {
-    return buffer;
+    if(size + sizeof(Block) > MEM_SIZE || size <= 0) {
+        return NULL;
+    }
+
+    Block* heap_start = (Block*) heap;
+
+    if(!heapHasBeenInitialized) {
+        heap_start->size = MEM_SIZE - sizeof(Block);
+        heap_start->next = NULL;
+        heap_start->isFree = TRUE;
+        heapHasBeenInitialized = TRUE;
+    }
+
+    Block* current = heap_start;
+
+    while(current) {
+        if(current->size >= size && current->isFree) {
+            if(current->size > size + sizeof(Block)) {
+                Block* new_block = (Block*) ((char*) current + sizeof(Block) + size);
+                new_block->size = current->size - size - sizeof(Block);
+                new_block->next = current->next;
+                new_block->isFree = TRUE;
+                current->next = new_block;
+            }
+
+            current->size = size;
+            current->isFree = FALSE;
+
+            return (char*) current + sizeof(Block);
+        }
+
+        current = current->next;
+    }
+
+    return NULL;
 }
 
 
-void my_free(void *ptr) {
+void myFree(void *ptr) {
 
 }

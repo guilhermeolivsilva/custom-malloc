f674ec4c6b45cfc61504b9e0942cb9a2332e4ed9
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2023-11-07 15:27:39 -0300

    test: added a main.c file to test the implementation

--- src/main.c
+++ src/main.c
@@ -2,13 +2,63 @@
 #include "mylib.h"
 
 
-int main() {
-    extern char buffer[SIZE];
-    int* teste = (int*) my_malloc(sizeof(int));
-    *teste = 10;
+int main()
+{
+    extern char heap[MEM_SIZE];
+    
+    printf("heap starts @ \t%p\n", heap);
+    printf("heap ends @ \t%p\n", heap + MEM_SIZE);
+    printf("available memory: %d\n\n", getAvailableMemory());
+    printf("----------------------------\n");
 
-    printf("teste: %d\n", *teste);
-    printf("buffer: %d\n", *buffer);
+    int* teste_int_1 = myMalloc(sizeof(int));
+    int* teste_int_2 = myMalloc(sizeof(int));
+    int* teste_int_3 = NULL;
+    double* teste_double_1 = NULL;
+
+    printf("teste_int_1 @ %p\n", teste_int_1);
+    printf("teste_int_2 @ %p\n", teste_int_2);
+    printf("teste_int_3 @ %p\n", teste_int_3);
+    printf("teste_double_1 @ %p\n", teste_double_1);
+    printf("available memory: %d\n\n", getAvailableMemory());
+
+    myFree(teste_int_1);
+    printf("Freed teste_int_1.\n");
+    printf("available memory: %d\n\n", getAvailableMemory());
+
+    printf("teste_int_1 @ %p\n", teste_int_1);
+    printf("teste_int_2 @ %p\n", teste_int_2);
+    printf("teste_int_3 @ %p\n", teste_int_3);
+    printf("teste_double_1 @ %p\n", teste_double_1);
+    printf("available memory: %d\n\n", getAvailableMemory());
+
+    teste_int_3 = myMalloc(sizeof(int));
+    printf("Allocated memory for teste_int_3.\n\n");
+
+    printf("teste_int_1 @ %p\n", teste_int_1);
+    printf("teste_int_2 @ %p\n", teste_int_2);
+    printf("teste_int_3 @ %p\n", teste_int_3);
+    printf("teste_double_1 @ %p\n", teste_double_1);
+    printf("available memory: %d\n\n", getAvailableMemory());
+
+    myFree(teste_int_2);
+    myFree(teste_int_3);
+    printf("Freed teste_int_2 and teste_int_3.\n\n");
+
+    printf("teste_int_1 @ %p\n", teste_int_1);
+    printf("teste_int_2 @ %p\n", teste_int_2);
+    printf("teste_int_3 @ %p\n", teste_int_3);
+    printf("teste_double_1 @ %p\n", teste_double_1);
+    printf("available memory: %d\n\n", getAvailableMemory());
+
+    teste_double_1 = myMalloc(sizeof(double));
+    printf("Allocated memory for teste_double_1.\n\n");
+
+    printf("teste_int_1 @ %p\n", teste_int_1);
+    printf("teste_int_2 @ %p\n", teste_int_2);
+    printf("teste_int_3 @ %p\n", teste_int_3);
+    printf("teste_double_1 @ %p\n", teste_double_1);
+    printf("available memory: %d\n\n", getAvailableMemory());
 
     return 0;
 }
